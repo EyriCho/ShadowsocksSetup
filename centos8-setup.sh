7c40cd1e7b94dba5c92655545bbc52dafd1470eb
Eyri Cho <eyricho@live.com>
2021-03-28 13:29:09 +0800

    feat: Edit setup.sh, change install method and second port configuration

--- centos8-setup.sh
+++ centos8-setup.sh
@@ -1,16 +1,23 @@
 echo "#"
 
 # ShadowsocksR Official site
-# https://github.com/shadowsocksrr
+# https://github.com/shadowsocks/shadowsocks-rust
 
-yum update
-yum install shadowsocks-libev
+# Download new official package
+# Check the new version from https://github.com/shadowsocks/shadowsocks-rust/releases
+# Update the new version link
+echo "Download package"
+curl -LJO shadowsocks-rust-gnu.tar.xz https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.10.1/shadowsocks-v1.10.1.x86_64-unknown-linux-gnu.tar.xz
+# must use J to unarchive
+tar -xJf shadowsocks-rust-gnu.tar.xz -C /usr/local/bin
+rm -f shadowsocks-rust-gnu.tar.xz
 
-echo "Install shadowsocks-libev finished"
+echo "Install shadowsocks-rust finished"
 
+echo "Enter port and password"
 while true
 do
-echo "Please enter a port for shadowsocks-libev"
+echo "Please enter a port for shadowsocks"
 read shadowsocksport
 expr ${shadowsocksport} + 1 &>/dev/null
 if [ $? -eq 0 ]; then
@@ -22,42 +29,78 @@ fi
 echo -e "Please enter a correct port number [1-65535]"
 done
 
-echo "Please enter password for shadowsocks-libev"
+echo "Please enter password for shadowsocks"
 read shadowsockspassword
 
-shadowsocks_libev_config="/etc/shadowsocks-libev/config.json"
-shadowsocks_libev_service="etc/systemd/system/shadowsocks-libev.service"
+echo "Enter second port and password"
+while true
+do
+echo "Please enter a port for shadowsocks"
+read shadowsocksport1
+expr ${shadowsocksport} + 1 &>/dev/null
+if [ $? -eq 0 ]; then
+    if [ ${shadowsocksport} -ge 1 ] && [ ${shadowsocksport} -le 65535 ] && [ ${shadowsocksport:0:1} != 0 ]; then
+        echo Enter port Success
+        break
+    fi
+fi
+echo -e "Please enter a correct port number [1-65535]"
+done
 
-cat > $(shadowsocks_libev_config) <<-EOF
+echo "Please enter password for shadowsocks"
+read shadowsockspassword1
+
+# Make config file
+shadowsocks_config="/etc/shadowsocks/config.json"
+shadowsocks_service="/etc/systemd/system/shadowsocks.service"
+
+if [ ! -d "$(dirname ${shadowsocks_config})" ]; then
+    mkdir -p $(dirname ${shadowsocks_config})
+fi
+
+cat > ${shadowsocks_config} <<-EOF
 {
-    "server":"0.0.0.0",
-    "server_port":"${shadowsocksport}",
-    “password":"${shadowsockspassword}",
-    "timeout":600,
-    "method":"chacha20",
-    "fast_open":false
+    "servers": [
+        {
+            "address":"127.0.0.1",
+            "port":${shadowsocksport},
+            “password":"${shadowsockspassword}",
+            "timeout":7200,
+            "method":"aes-256-gcm"
+        },
+        {
+            "address":"127.0.0.1",
+            "port":${shadowsocksport1},
+            “password":"${shadowsockspassword1}",
+            "timeout":7200,
+            "method":"chacha20-ietf-poly1305"
+        }
+    ]
 }
 EOF
 
-cat > $(shadowsocks_libev_service) <<-EOF
+cat > ${shadowsocks_service} <<-EOF
 [Unit]
-Description=shadowsocks-libev server
+Description=shadowsocks server
 After=network.target
 
 [Service]
 TimeoutStartSec=0
 Type=simple
-ExecStart=/usr/local/bin/ss-server -c /etc/shadowsocks-libev/config.json
+ExecStart=/usr/local/bin/ssserver -c ${shadowsocks_config}
 Restart=on-abort
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-systemctl start shadwosocks_libev
-systemctl enable shadwosocks_libev
+# 
+systemctl start shadwosocks
+systemctl enable shadwosocks
 
 default_zone=$(firewall-cmd --get-default-zone)
 firewall-cmd --permanent --zone=${default_zone} --add-port=${shadowsocksport}/tcp
 firewall-cmd --permanent --zone=${default_zone} --add-port=${shadowsocksport}/udp
+firewall-cmd --permanent --zone=${default_zone} --add-port=${shadowsocksport1}/tcp
+firewall-cmd --permanent --zone=${default_zone} --add-port=${shadowsocksport1}/udp
 firewall-cmd --reload
